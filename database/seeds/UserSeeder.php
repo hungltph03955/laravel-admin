d411ca95570627dba94f3594db9efe862870195b
tuanhung <mac@Megalodon.local>
2020-09-03 23:04:05 +0700

    fix conflig

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,6 +11,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class, 2000)->create();
+        factory(\App\User::class, 3000)->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,10 @@ use Illuminate\Support\Facades\Route;
 //Route::put('users/{id}', 'UserController@update');
 //Route::delete('users/{id}', 'UserController@destroy');
 
-Route::apiResource('users', 'UserController');
+
+Route::post('login', [ 'as' => 'login', 'uses' => 'AuthController@login']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::apiResource('users', 'UserController');
+});
+
